8decd6681580820e61ed4f95807abaae823533ff
Amrit Sharma <2005amritsharma@gmail.com>
2011-12-31 00:01:34 -0800

    just created a skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Points::Application.config.secret_token = 'b37c7cabbf0a1410f8152fc532e8068e9eef2036882de21b618bb45f3f79c576aa266b9014b7d125d7f62ed80288ff522af9d9dcd05fd2f4e99b5512c99e1a63'
